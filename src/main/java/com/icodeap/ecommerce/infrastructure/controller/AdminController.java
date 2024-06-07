5741652456ffef951e9f2979b15c0a285761251c
Roberto Sánchez <rsa568422@gmail.com>
2024-06-07 18:33:49 +0200

    37. Mostrar los productos para el administrador en la home

--- src/main/java/com/icodeap/ecommerce/infrastructure/controller/AdminController.java
+++ src/main/java/com/icodeap/ecommerce/infrastructure/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.icodeap.ecommerce.infrastructure.controller;
 
+import com.icodeap.ecommerce.application.service.ProductService;
+import com.icodeap.ecommerce.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final ProductService productService;
+
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        var user = new User();
+        user.setId(1);
+        var products = productService.getProductsByUser(user);
+        model.addAttribute("products", products);
         return "admin/home";
     }
 }
