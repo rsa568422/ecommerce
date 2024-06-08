7f10068061abc86635eedadd09a0c2bd598a8858
Roberto Sánchez <rsa568422@gmail.com>
2024-06-08 16:35:32 +0200

    59. Crear los mapper para convertir de OrderProductEntity a OrderProduct

--- src/main/java/com/icodeap/ecommerce/infrastructure/mapper/OrderProductMapper.java
+++ src/main/java/com/icodeap/ecommerce/infrastructure/mapper/OrderProductMapper.java
@@ -4,12 +4,19 @@ import com.icodeap.ecommerce.domain.OrderProduct;
 import com.icodeap.ecommerce.infrastructure.entity.OrderProductEntity;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {OrderMapper.class, ProductMapper.class})
 public interface OrderProductMapper {
 
+    @Mappings({
+            @Mapping(source = "pk.order", target = "order"),
+            @Mapping(source = "pk.product", target = "product"),
+            @Mapping(source = "quantity", target = "quantity")
+    })
     OrderProduct toOrderProduct(OrderProductEntity orderProductEntity);
 
     Iterable<OrderProduct> toOrderProducts(Iterable<OrderProductEntity> orderProductEntities);
