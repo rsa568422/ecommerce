db6b988c90a4cbad12e7bd0df70137142fffde90
Roberto Sánchez <rsa568422@gmail.com>
2024-06-08 14:22:38 +0200

    54. Mostrar los productos en la home del usuario y ver los detalles de producto

--- src/main/java/com/icodeap/ecommerce/infrastructure/controller/HomeController.java
+++ src/main/java/com/icodeap/ecommerce/infrastructure/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.icodeap.ecommerce.infrastructure.controller;
 
+import com.icodeap.ecommerce.application.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeController {
 
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("products", productService.getProducts());
         return "home";
+    }
+
+    @GetMapping("/product-detail/{id}")
+    public String productDetail() {
+        return "user/productdetail";
     }
 }
