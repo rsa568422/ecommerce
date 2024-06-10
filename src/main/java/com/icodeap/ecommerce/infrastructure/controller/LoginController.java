02c784252eec876f0675cc8cc49cb7fcddf82bd6
Roberto Sánchez <rsa568422@gmail.com>
2024-06-10 22:57:55 +0200

    77. Funcionalidad para la autenticación temporal y manejo de sesión de usuario

--- src/main/java/com/icodeap/ecommerce/infrastructure/controller/LoginController.java
+++ src/main/java/com/icodeap/ecommerce/infrastructure/controller/LoginController.java
@@ -1,15 +1,44 @@
 package com.icodeap.ecommerce.infrastructure.controller;
 
+import com.icodeap.ecommerce.application.service.LoginService;
+import com.icodeap.ecommerce.domain.UserType;
+import com.icodeap.ecommerce.infrastructure.dto.UserDTO;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
     @GetMapping
     public String login() {
         return "login";
+    }
+
+    @PostMapping
+    public String access(UserDTO userDTO, HttpSession httpSession) {
+        userDTO.setEmail(userDTO.getUsername());
+        log.info("userDTO.email: {}", userDTO.getEmail());
+        log.info("userDTO.pass: {}", userDTO.getPassword());
+        if (loginService.existUser(userDTO)) {
+            httpSession.setAttribute("iduser", loginService.getUserId(userDTO.getEmail()));
+            if (UserType.ADMIN.equals(loginService.getUserType(userDTO))) {
+                return "redirect:/admin";
+            } else {
+                return "redirect:/home";
+            }
+        }
+        return "redirect:/home";
     }
 }
