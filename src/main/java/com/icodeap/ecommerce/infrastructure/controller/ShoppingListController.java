89e4a0826045f9c1eae51367dafed22792e982ea
Roberto Sánchez <rsa568422@gmail.com>
2024-06-30 14:16:09 +0200

    90. Obtener los detalles de la orden para la presentación

--- src/main/java/com/icodeap/ecommerce/infrastructure/controller/ShoppingListController.java
+++ src/main/java/com/icodeap/ecommerce/infrastructure/controller/ShoppingListController.java
@@ -1,7 +1,9 @@
 package com.icodeap.ecommerce.infrastructure.controller;
 
+import com.icodeap.ecommerce.application.service.OrderProductService;
 import com.icodeap.ecommerce.application.service.OrderService;
 import com.icodeap.ecommerce.application.service.UserService;
+import com.icodeap.ecommerce.domain.Order;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,14 +18,27 @@ public class ShoppingListController {
 
     private final UserService userService;
 
-    public ShoppingListController(OrderService orderService, UserService userService) {
+    private final OrderProductService orderProductService;
+
+    public ShoppingListController(OrderService orderService,
+                                  UserService userService,
+                                  OrderProductService orderProductService) {
         this.orderService = orderService;
         this.userService = userService;
+        this.orderProductService = orderProductService;
     }
 
     @GetMapping
     public String showShoppingList(Model model, HttpSession httpSession) {
         var user = userService.findById(Integer.parseInt(httpSession.getAttribute("iduser").toString()));
+        var orders = orderService.getOrdersByUser(user);
+        orders.forEach(this::getOrderProducts);
+        model.addAttribute("orders", orders);
         return "user/shoppinglist";
+    }
+
+    private void getOrderProducts(Order order) {
+        var products = orderProductService.getOrderProductsByOrder(order);
+        order.addOrdersProduct(products);
     }
 }
